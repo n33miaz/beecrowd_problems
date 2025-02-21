6a17365a8eae8673aa1d36a41c1d1f831e1b58f6
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2025-02-21 20:00:51 -0300

    Update 1045_tipos_de_triangulos.js

--- 1045_tipos_de_triangulos.js
+++ 1045_tipos_de_triangulos.js
@@ -1,31 +1,37 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split(' ');
 
-const A = parseInt(lines[0]); // 7
-const B = parseInt(lines[1]); // 5
-const C = parseInt(lines[2]); // 7
+let valores = [parseFloat(lines[0]), parseFloat(lines[1]), parseFloat(lines[2])]
 
-if (A >= B + C)
+valores.sort((a, b) => b - a)
+
+const A = valores[0];
+const B = valores[1];
+const C = valores[2];
+
+if (A >= B + C) 
 {
-    console.log("NAO FORMA TRIANGULO")
-}
-else if (Math.pow(A, 2) === Math.pow(B, 2) + Math.pow(C, 2))
+    console.log("NAO FORMA TRIANGULO");
+} 
+else 
 {
-    console.log("TRIANGULO RETANGULO")
-}
-else if (Math.pow(A, 2) > Math.pow(B, 2) + Math.pow(C, 2))
-{
-    console.log("TRIANGULO OBTUSANGULO")
-}
-else if (Math.pow(A, 2) < Math.pow(B, 2) + Math.pow(C, 2))
-{
-    console.log("TRIANGULO ACUTANGULO")
-}
-else if (A === B === C)
-{
-    console.log("TRIANGULO EQUILATERO")
-}
-else if (A === B || B === C || A === C )
-{
-    console.log("TRIANGULO ISOSCELES")
+    if (Math.pow(A, 2) === Math.pow(B, 2) + Math.pow(C, 2)) 
+    {
+        console.log("TRIANGULO RETANGULO");
+    }
+    if (Math.pow(A, 2) > Math.pow(B, 2) + Math.pow(C, 2)) 
+    {
+        console.log("TRIANGULO OBTUSANGULO");
+    }
+    if (Math.pow(A, 2) < Math.pow(B, 2) + Math.pow(C, 2)) 
+    {
+        console.log("TRIANGULO ACUTANGULO");
+    }
+    if (A === B && B === C) {
+        console.log("TRIANGULO EQUILATERO");
+    } 
+    else if (A === B || B === C || A === C) 
+    {
+        console.log("TRIANGULO ISOSCELES");
+    }
 }
